d2bf65a0a844695e7bd78384bfb0657b971476eb
norch2009 <norch853@gmail.com>
2025-06-17 21:45:14 +0800

    Update getBotInitialData.js

--- src/getBotInitialData.js
+++ src/getBotInitialData.js
@@ -1,43 +1,51 @@
 "use strict";
 
 const utils = require("../utils");
-// @NethWs3Dev
 
 module.exports = (defaultFuncs, api, ctx) => {
   return async (callback) => {
-  let resolveFunc = () => {};
-  let rejectFunc = () => {};
-  const returnPromise = new Promise((resolve, reject) => {
+    let resolveFunc = () => {};
+    let rejectFunc = () => {};
+    const returnPromise = new Promise((resolve, reject) => {
       resolveFunc = resolve;
       rejectFunc = reject;
-  });
-  if (!callback) {
+    });
+
+    if (!callback) {
       callback = (err, data) => {
-        if (err) {
-          return rejectFunc(err);
-        }
+        if (err) return rejectFunc(err);
         resolveFunc(data);
       };
-  }
-  utils.log("Fetching account info...");
-  api.httpGet(`https://www.facebook.com/profile.php?id=${ctx.userID}`, null, {
-    customUserAgent: utils.windowsUserAgent
-  }, (err, data) => {
-      if (err) throw err;
-      const profileMatch = data.match(/"CurrentUserInitialData",\[\],\{(.*?)\},(.*?)\]/);
-      if (profileMatch && profileMatch[1]){
+    }
+
+    utils.log("Fetching account info...");
+
+    try {
+      const res = await defaultFuncs.get(
+        `https://www.facebook.com/profile.php?id=${ctx.userID}`,
+        ctx.jar,
+        null,
+        ctx,
+        { customUserAgent: utils.windowsUserAgent }
+      );
+
+      const profileMatch = res.body.match(/"CurrentUserInitialData",\[\],\{(.*?)\},(.*?)\]/);
+      if (profileMatch && profileMatch[1]) {
         const accountJson = JSON.parse(`{${profileMatch[1]}}`);
         accountJson.name = accountJson.NAME;
         accountJson.uid = accountJson.USER_ID;
         delete accountJson.NAME;
         delete accountJson.USER_ID;
+        return callback(null, accountJson);
+      } else {
         return callback(null, {
-          ...accountJson
-      });
-      } else return callback(null, { error: "Something went wrong. Maybe its possible that it has a limitation due to spam requests. You can try again later." });
-  }, true);
-  return returnPromise;
+          error: "❌ Failed to parse profile. Maybe limited or spam blocked. Try again later."
+        });
+      }
+    } catch (err) {
+      return callback(err);
+    }
+
+    return returnPromise;
   };
 };
- 
- 
